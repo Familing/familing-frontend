2ecfd41c7be58c1830247650104e4e11dff2c138
rudtj <kyoungseo03@naver.com>
2024-07-29 14:23:31 +0900

    feat: 전체 Banner 구현

---
+++ src/components/features/Home/Banner/Banner.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import Swiper from 'react-native-swiper';
+import {FamilingBanner} from './FamilingBanner';
+import {CardBanner} from './CardBanner';
+import {ChatBanner} from './ChatBanner';
+
+export const Banner = () => {
+  return (
+    <Swiper
+      showsButtons
+      loop={false}
+      dot={<View style={styles.dot} />}
+      activeDot={<View style={styles.activeDot} />}
+      index={0}>
+      <FamilingBanner />
+      <CardBanner />
+      <ChatBanner />
+    </Swiper>
+  );
+};
+
+const styles = StyleSheet.create({
+  dot: {
+    backgroundColor: '#FFFFFF',
+    width: 4,
+    height: 4,
+    borderRadius: 4,
+    margin: 3,
+  },
+  activeDot: {
+    backgroundColor: '#FFBE00',
+    width: 20,
+    height: 4,
+    borderRadius: 16,
+    margin: 3,
+  },
+});
