46208dd6452f1187de3b0e630131a78c7ab530f8
rudtj <kyoungseo03@naver.com>
2024-07-29 14:23:20 +0900

    feat: ChatBanner 구현

---
+++ src/components/features/Home/Banner/ChatBanner.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+export const ChatBanner = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Familing만의</Text>
+        <Text style={styles.subtitle}>공감봇과 애정봇</Text>
+        <Text style={styles.description}>AI가 도와드릴게요!</Text>
+      </View>
+      <Image
+        source={require('../../../../src/assets/images/CardImage.png')}
+        style={styles.image}
+        resizeMode="contain"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#F8F8F8',
+    width: '360px',
+    height: '210px',
+    padding: '24px',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textContainer: {
+    marginBottom: 24,
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 800,
+    color: '#4D83F4',
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: 800,
+    color: '#383838',
+  },
+  description: {
+    fontSize: 12,
+    fontWeight: 500,
+    color: '#383838',
+  },
+  image: {
+    width: '190px',
+    height: '110px',
+    marginRight: 24,
+  },
+});
