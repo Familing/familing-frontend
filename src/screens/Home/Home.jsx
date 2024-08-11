981e3ac3f3c9880ac507be05ce15e7e190fdcf24
yunseong0404 <333665@naver.com>
2024-08-11 17:23:45 +0900

    fix: Home screen 수정

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { CustomHeader } from '../../components/features/Layout/BottomTabScreen'
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function Home() {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
     </SafeAreaView>
-  )
+  );
 }
-
