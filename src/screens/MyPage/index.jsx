a24fb45254a5c7caabc713bb03fa5112ae71675b
rudtj <kyoungseo03@naver.com>
2024-08-13 15:31:39 +0900

    design: css 수정

--- src/screens/MyPage/index.jsx
+++ src/screens/MyPage/index.jsx
@@ -14,7 +14,6 @@ import camera from '@assets/images/register/camera.png';
 import gallery from '@assets/images/register/gallery.png';
 import clearbtn from '@assets/images/button/clearbtn.png';
 
-
 export default function MyPage({navigation}) {
   const [modalVisible, setModalVisible] = useState(false);
 
@@ -38,47 +37,60 @@ export default function MyPage({navigation}) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>마이페이지</Text>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>마이페이지</Text>
+      </View>
 
       <View style={styles.profileImageContainer} onPress={openModal}>
         <Image style={styles.profileImage1} source={photocard1} />
         <Image style={styles.profileImage2} source={switchbtn} />
-
       </View>
 
       <View style={styles.profileContainer}>
-        <Text style={styles.nicknameTitle1}>닉네임</Text>
-        <Text style={styles.nicknameText1}>내가 둘째다</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('NicknameScreen')}>
-          <Image style={styles.arrowButton1} source={arrowbtn} />
-
-        </TouchableOpacity>
+        <View style={styles.nicknameContainer}>
+          <Text style={styles.nicknameTitle1}>닉네임</Text>
+          <Text style={styles.nicknameText1}>내가 둘째다</Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('NicknameScreen')}>
+            <Image style={styles.arrowButton1} source={arrowbtn} />
+          </TouchableOpacity>
+        </View>
         <View style={styles.separator1} />
-        <Text style={styles.nicknameTitle2}>이름</Text>
-        <Text style={styles.nicknameText2}>진서현</Text>
+        <View style={styles.nameContainer}>
+          <Text style={styles.nicknameTitle2}>이름</Text>
+          <Text style={styles.nicknameText2}>진서현</Text>
+        </View>
       </View>
 
       <View style={styles.profileContainer2}>
-        <Text style={styles.nicknameTitle3}>우리 가족</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('MyFamilyScreen')}>
-          <Image style={styles.arrowButton2} source={arrowbtn} />
-        </TouchableOpacity>
+        <View style={styles.myfamilyContainer}>
+          <Text style={styles.nicknameTitle3}>우리 가족</Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('MyFamilyScreen')}>
+            <Image style={styles.arrowButton2} source={arrowbtn} />
+          </TouchableOpacity>
+        </View>
         <View style={styles.separator2} />
-        <Text style={styles.nicknameTitle4}>구독 모델</Text>
-        <Text style={styles.nicknameText3}>프리미엄형</Text>
-        <Image style={styles.arrowButton3} source={arrowbtn} />
+        <View style={styles.subscribeContainer}>
+          <Text style={styles.nicknameTitle4}>구독 모델</Text>
+          <Text style={styles.nicknameText3}>프리미엄형</Text>
+          <Image style={styles.arrowButton3} source={arrowbtn} />
+        </View>
         <View style={styles.separator3} />
-        <Text style={styles.nicknameTitle5}>결제 관리</Text>
-        <Image style={styles.arrowButton4} source={arrowbtn} />
+        <View style={styles.payContainer}>
+          <Text style={styles.nicknameTitle5}>결제 관리</Text>
+          <Image style={styles.arrowButton4} source={arrowbtn} />
+        </View>
       </View>
 
       <View style={styles.profileContainer3}>
-        <Text style={styles.nicknameTitle6}>스냅샷 시간 설정</Text>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('SnapshotTimeScreen')}>
-          <Image style={styles.arrowButton5} source={arrowbtn} />
-
-        </TouchableOpacity>
+        <View style={styles.snapshotContainer}>
+          <Text style={styles.nicknameTitle6}>스냅샷 시간 설정</Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SnapshotTimeScreen')}>
+            <Image style={styles.arrowButton5} source={arrowbtn} />
+          </TouchableOpacity>
+        </View>
       </View>
 
       <Modal
@@ -110,198 +122,164 @@ export default function MyPage({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
+    width: 360,
+    height: 800,
+  },
+  headerContainer: {
+    justifyContent: 'flex-start',
+    marginLeft: 24,
+    marginTop: 20,
   },
   header: {
     fontSize: 20,
     fontWeight: '800',
     color: '#383838',
-    position: 'absolute',
-    top: 20,
-    left: 24,
+    justifyContent: 'flex-start',
   },
   profileImageContainer: {
+    justifyContent: 'center',
     width: 92,
     height: 92,
-    position: 'absolute',
-    top: 139,
-    left: 134,
+    marginTop: 75,
+    marginLeft: 134,
   },
   profileImage1: {
     width: 92,
     height: 92,
-    position: 'absolute',
-    top: 139,
-    left: 134,
   },
   profileImage2: {
     width: 28,
     height: 28,
     position: 'absolute',
-    top: 203,
-    left: 198,
+    bottom: 0,
+    right: 0,
   },
   profileContainer: {
     width: 312,
     height: 121,
-    position: 'absolute',
-    top: 259,
-    left: 24,
     borderRadius: 12,
-    border: 1,
+    borderWidth: 1,
     borderColor: '#E7E7E7',
+    marginTop: 37,
+    marginLeft: 24,
+  },
+  nicknameContainer: {
+    flexDirection: 'row',
   },
   nicknameTitle1: {
     fontSize: 16,
     fontWeight: '700',
     color: '#383838',
-    position: 'absolute',
-    top: 280,
-    left: 36,
+    marginTop: 21,
+    marginLeft: 12,
   },
   nicknameText1: {
     fontSize: 16,
     fontWeight: '400',
     color: '#B3B3B3',
-    position: 'absolute',
-    top: 280,
-    left: 228,
+    marginTop: 21,
+    marginLeft: 150,
   },
   arrowButton1: {
     width: 18,
     height: 18,
-    position: 'absolute',
-    top: 299,
-    left: 324,
+    marginTop: 25,
   },
   separator: {
     width: 310,
     border: 1,
-    position: 'absolute',
-    top: 320,
-    left: 25,
     borderColor: '#E7E7E7',
+  },
+  nameContainer: {
+    flexDirection: 'row',
   },
   nicknameTitle2: {
     fontSize: 16,
     fontWeight: '700',
     color: '#383838',
-    position: 'absolute',
-    top: 340,
-    left: 36,
   },
   nicknameText2: {
     fontSize: 16,
     fontWeight: '400',
     color: '#B3B3B3',
-    position: 'absolute',
-    top: 340,
-    left: 282,
   },
   profileContainer2: {
     width: 312,
     height: 181,
-    position: 'absolute',
-    top: 400,
-    left: 24,
     borderRadius: 12,
-    border: 1,
+    borderWidth: 1,
     borderColor: '#E7E7E7',
+    marginTop: 20,
+  },
+  myfamilyContainer: {
+    flexDirection: 'row',
   },
   nicknameTitle3: {
     fontSize: 16,
     fontWeight: '700',
     color: '#383838',
-    position: 'absolute',
-    top: 421,
-    left: 36,
   },
   arrowButton2: {
     width: 18,
     height: 18,
-    position: 'absolute',
-    top: 440,
-    left: 324,
   },
   separator2: {
     width: 311,
     border: 1,
-    position: 'absolute',
-    top: 461,
-    left: 24,
     borderColor: '#E7E7E7',
+  },
+  subscribeContainer: {
+    flexDirection: 'row',
   },
   nicknameTitle4: {
     fontSize: 16,
     fontWeight: '700',
     color: '#383838',
-    position: 'absolute',
-    top: 481,
-    left: 37,
   },
   nicknameText3: {
     fontSize: 16,
     fontWeight: '400',
     color: '#B3B3B3',
-    position: 'absolute',
-    top: 481,
-    left: 232,
   },
   arrowButton3: {
     width: 18,
     height: 18,
-    position: 'absolute',
-    top: 500,
-    left: 324,
   },
   separator3: {
     width: 311,
     border: 1,
-    position: 'absolute',
-    top: 521,
-    left: 24,
     borderColor: '#E7E7E7',
+  },
+  payContainer: {
+    flexDirection: 'row',
   },
   nicknameTitle5: {
     fontSize: 16,
     fontWeight: '700',
     color: '#383838',
-    position: 'absolute',
-    top: 541,
-    left: 37,
   },
   arrowButton4: {
     width: 18,
     height: 18,
-    position: 'absolute',
-    top: 560,
-    left: 324,
   },
   profileContainer3: {
     width: 312,
     height: 61,
-    position: 'absolute',
-    top: 601,
-    left: 24,
     borderRadius: 12,
-    border: 1,
+    borderWidth: 1,
     borderColor: '#E7E7E7',
+    marginTop: 20,
+  },
+  snapshotContainer: {
+    flexDirection: 'row',
   },
   nicknameTitle6: {
     fontSize: 16,
     fontWeight: '700',
     color: '#383838',
-    position: 'absolute',
-    top: 622,
-    left: 37,
   },
   arrowButton5: {
     width: 18,
-    height: 18,
-    position: 'absolute',
-    top: 641,
-    left: 324,
   },
 });
