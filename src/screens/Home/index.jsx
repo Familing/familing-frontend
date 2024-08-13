737bb43c11c959f892095d321ef838a042838b8c
rudtj <kyoungseo03@naver.com>
2024-08-13 22:20:51 +0900

    fix: 태그 수정

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 
 export default function Home({navigation}) {
   return (
