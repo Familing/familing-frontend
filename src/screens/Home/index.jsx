2ea8ee6cac90630b7d6a9ed67cfbb95d81dd9611
yunseong0404 <333665@naver.com>
2024-08-14 10:18:34 +0900

    fix: Home 구현

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,17 @@
+import {FamilingBanner} from '@/components/features/Home/Banner/FamilingBanner';
+import {SnapShot} from '@/components/features/Home/SnapShot/SnapShot';
+import StatusBorad from '@/components/features/Home/StatusBoard/StatusBoard';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView, View} from 'react-native';
 
 export default function Home({navigation}) {
   return (
     <View>
-      <Text>Home</Text>
+      <ScrollView>
+        <FamilingBanner />
+        <StatusBorad />
+        <SnapShot />
+      </ScrollView>
     </View>
   );
 }
