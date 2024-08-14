2b81fb136a1b1c942d8d3b9e3750a8d8b3d7a0b9
yunseong0404 <333665@naver.com>
2024-08-14 10:17:24 +0900

    design: PhotoCard 수정

--- src/components/features/Home/SnapShot/PhotoCard.jsx
+++ src/components/features/Home/SnapShot/PhotoCard.jsx
@@ -2,22 +2,33 @@ import React from 'react';
 import {View, Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import plusbtn from '../../../../assets/images/photocard/plusbtn.png';
 
-export const PhotoCard = ({isAddButton}) => {
+export const PhotoCard = ({isAddButton, imageSource}) => {
   return (
-    <TouchableOpacity style={styles.card}>
-      {isAddButton ? (
-        <Image source={plusbtn} style={styles.addImage} />
-      ) : (
-        <View style={styles.content}>
-          <Text style={styles.description}>아직 업로드 전이에요!</Text>
-        </View>
-      )}
-    </TouchableOpacity>
+    <View>
+      <TouchableOpacity style={styles.card}>
+        {isAddButton ? (
+          <Image source={plusbtn} style={styles.addImage} />
+        ) : (
+          <View style={styles.content}>
+            <Text style={styles.description}>아직 업로드 전이에요!</Text>
+          </View>
+        )}
+      </TouchableOpacity>
+      <Image source={imageSource} style={styles.profile} />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  profile: {
+    position: 'absolute',
+    top: 4,
+    left: 4,
+    width: 38,
+    height: 38,
+  },
   card: {
+    display: 'flex',
     width: 150,
     height: 150,
     backgroundColor: '#E7E7E7',
@@ -26,17 +37,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   content: {
-    width: 150,
-    height: 150,
+    width: '100%',
+    height: '100%',
     backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   description: {
     fontSize: 12,
     fontWeight: '700',
-    color: '#C5C5C5',
+    color: '#CECECE',
   },
   addImage: {
-    width: '21',
-    height: '21',
+    width: 21,
+    height: 21,
   },
 });
