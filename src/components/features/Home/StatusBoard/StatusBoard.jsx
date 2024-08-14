f29a0a98d3ad2f1c97c743fc5b6f9d517b77073b
yunseong0404 <333665@naver.com>
2024-08-14 10:16:26 +0900

    design: StatusBorad style 수정

--- src/components/features/Home/StatusBoard/StatusBoard.jsx
+++ src/components/features/Home/StatusBoard/StatusBoard.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import StatusProfile from './StatusProfile';
 import {Dad_profile} from '../../../icon/Dad_profile';
+import {Daughter_profile} from '@/components/icon/Daughter_profile';
+import {Mom_profile} from '@/components/icon/Mom_profile';
+import {Son_profile} from '@/components/icon/Son_profile';
 
 export default function StatusBorad() {
   return (
-    <View style={{backgroundColor: 'orange'}}>
+    <View style={styles.wrapper}>
       <View style={styles.container}>
         <Text style={styles.title}>상태보기</Text>
         <Text style={styles.subTitle}>현재 가족들의 상태를 볼 수 있어요.</Text>
         <StatusProfile Profile={Dad_profile} name="행복한 부자아빠" />
+        <StatusProfile Profile={Mom_profile} name="익순여왕님" />
+        <StatusProfile Profile={Daughter_profile} name="민지 공주" />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#fff',
+  },
   container: {
-    backgroundColor: 'green',
     marginTop: 24,
     marginHorizontal: 24,
   },
@@ -30,6 +37,8 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: '400',
     color: '#383838',
-    marginTop: 3.89
+    marginTop: 3.89,
+    marginBottom: 15,
   },
+
 });
