7d32f2b0191eb492b4717d871f4b7be758cc22f9
yunseong0404 <333665@naver.com>
2024-08-24 18:27:28 +0900

    feat: header 추가

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,17 +1,27 @@
-import {FamilingBanner} from '@/components/features/Home/Banner/FamilingBanner';
-import {SnapShot} from '@/components/features/Home/SnapShot/SnapShot';
-import StatusBorad from '@/components/features/Home/StatusBoard/StatusBoard';
-import React from 'react';
-import {ScrollView, View} from 'react-native';
+import {FamilingBanner} from '../../components/features/Home/Banner/FamilingBanner';
+import {SnapShot} from '../../components/features/Home/SnapShot/SnapShot';
+import StatusBorad from '../../components/features/Home/StatusBoard/StatusBoard';
+import {CustomHeader} from '../../components/features/Layout/CustomHeader';
+import React, {useEffect} from 'react';
+import {ScrollView, StyleSheet, View} from 'react-native';
 
-export default function Home({navigation}) {
+export default function Home() {
   return (
     <View>
+      <CustomHeader />
       <ScrollView>
         <FamilingBanner />
         <StatusBorad />
         <SnapShot />
+        <View style={styles.space} />
       </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  space: {
+    height: 64,
+    width: '100%',
+  },
+});
