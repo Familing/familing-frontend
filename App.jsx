cd72420a24bdc5e1c5d8293f3caee722eaf845e1
yunseong0404 <333665@naver.com>
2024-08-27 15:35:23 +0900

    feat: message 리스너 구현

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {BottomTabScreen} from './src/navigation/BottomTabScreen.jsx';
-import {PermissionsAndroid, Platform, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
 import {StartStacks} from '@/navigation/StartStack.jsx';
 import messaging from '@react-native-firebase/messaging';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -11,6 +11,11 @@ function App() {
   const Stack = createNativeStackNavigator();
   useEffect(() => {
     getFcmToken();
+
+    foregroundListener();
+    return () => {
+      foregroundListener();
+    };
   }, []);
 
   //FCM 토큰 발급
@@ -21,6 +26,21 @@ function App() {
     await AsyncStorage.setItem('fcmToken', fcmToken);
     console.log('[+] FCM Token :: ', fcmToken);
   };
+
+  //foreground 메시지 리스너
+  const foregroundListener = messaging().onMessage(async remoteMessage => {
+    console.log('[+] Remote Message', JSON.stringify(remoteMessage));
+  });
+
+  //background 메시지 리스너
+  const backgroundListener = messaging().setBackgroundMessageHandler(
+    async remoteMessage => {
+      console.log(
+        '[+] Message in the background',
+        JSON.stringify(remoteMessage),
+      );
+    },
+  );
 
   return (
     <NavigationContainer>
