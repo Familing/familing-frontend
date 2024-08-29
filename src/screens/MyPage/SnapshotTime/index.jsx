b7dd0d36c7f2a5e02cfeed1b34a70481b1598a2d
rudtj <kyoungseo03@naver.com>
2024-08-30 03:32:24 +0900

    refactor: SnapshotTime 로직 변경

--- src/screens/MyPage/SnapshotTime/index.jsx
+++ src/screens/MyPage/SnapshotTime/index.jsx
@@ -5,10 +5,12 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
+  Image,
 } from 'react-native';
+import Arrow from '@assets/images/register/arrowImg.png';
 
 export default function SnapshotTimeScreen({navigation}) {
-  const [selectedTime, setSelectedTime] = useState(null);
+  const [selectedTime, setSelectedTime] = useState('10:00');
 
   const times = [
     {label: '8:00', key: '8:00'},
@@ -16,40 +18,44 @@ export default function SnapshotTimeScreen({navigation}) {
     {label: '10:00', key: '10:00'},
     {label: '11:00', key: '11:00'},
     {label: '12:00', key: '12:00'},
-    {label: '1:00', key: '1:00'},
-    {label: '2:00', key: '2:00'},
-    {label: '4:00', key: '4:00'},
-    {label: '6:00', key: '6:00'},
-    {label: '8:00', key: '8:00'},
+    {label: '13:00', key: '13:00'},
+    {label: '14:00', key: '14:00'},
+    {label: '15:00', key: '15:00'},
+    {label: '16:00', key: '16:00'},
+    {label: '17:00', key: '17:00'},
   ];
 
   const handleTimePress = time => {
     setSelectedTime(time);
   };
 
-  const renderTimeButtons = () => {
+  const renderTimeButtons = timesArray => {
+    return timesArray.map(time => (
+      <TouchableOpacity
+        key={time.key}
+        style={[
+          styles.timeButton,
+          selectedTime === time.key && styles.selectedTime,
+        ]}
+        onPress={() => handleTimePress(time.key)}>
+        <Text
+          style={
+            selectedTime === time.key
+              ? styles.selectedTimeText
+              : styles.defaultText
+          }>
+          {time.label}
+        </Text>
+      </TouchableOpacity>
+    ));
+  };
+
+  const renderTimeRows = timesArray => {
     const rows = [];
-    for (let i = 0; i < times.length; i += 3) {
+    for (let i = 0; i < timesArray.length; i += 3) {
       rows.push(
         <View style={styles.timeRow} key={i}>
-          {times.slice(i, i + 3).map(time => (
-            <TouchableOpacity
-              key={time.key}
-              style={[
-                styles.timeButton,
-                selectedTime === time.key && styles.selectedTime,
-              ]}
-              onPress={() => handleTimePress(time.key)}>
-              <Text
-                style={
-                  selectedTime === time.key
-                    ? styles.selectedTimeText
-                    : styles.defaultText
-                }>
-                {time.label}
-              </Text>
-            </TouchableOpacity>
-          ))}
+          {renderTimeButtons(timesArray.slice(i, i + 3))}
         </View>,
       );
     }
@@ -58,21 +64,25 @@ export default function SnapshotTimeScreen({navigation}) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>스냅샷 시간 설정</Text>
+      <View style={styles.titleContainer}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image source={Arrow} style={styles.arrowImage} />
+        </TouchableOpacity>
+        <Text style={styles.title}>스냅샷 시간 설정</Text>
+      </View>
       <Text style={styles.subtitle}>주제 알림 받을 시간</Text>
 
       <ScrollView>
         <View style={styles.sectionBox1}>
-          <Text style={styles.sectionTitle1}>•</Text>
+          <Text style={styles.sectionTitle1}>• </Text>
           <Text style={styles.sectionTitle2}>오전</Text>
         </View>
-        {renderTimeButtons().slice(0, 4)}
-
-        <View style={styles.sectionBox2}>
+        {renderTimeRows(times.slice(0, 4))}
+        <View style={styles.sectionBox1}>
           <Text style={styles.sectionTitle1}>•</Text>
           <Text style={styles.sectionTitle2}>오후</Text>
         </View>
-        {renderTimeButtons().slice(4)}
+        {renderTimeRows(times.slice(4))}
       </ScrollView>
 
       <TouchableOpacity style={styles.changeButton}>
@@ -87,34 +97,39 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  titleContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    gap: 75,
+  },
+  arrowImage: {
+    width: 20,
+    height: 15,
+    marginLeft: 24,
+  },
   title: {
     fontSize: 20,
     fontWeight: '800',
     color: '#383838',
-    position: 'absolute',
-    top: 20,
-    left: 114,
+    marginTop: -5,
   },
   subtitle: {
     fontSize: 16,
     fontWeight: '800',
     color: '#383838',
-    marginTop: 60,
+    marginTop: 50,
     marginLeft: 24,
   },
   sectionBox1: {
-    width: 36,
-    height: 20,
     flexDirection: 'row',
     gap: 4,
-    position: 'absolute',
-    top: 137,
-    left: 24,
+    marginTop: 10,
+    marginLeft: 24,
   },
   sectionTitle1: {
-    width: 4,
-    height: 4,
+    fontSize: 20,
     color: '#383838',
+    lineHeight: 25,
   },
   sectionTitle2: {
     fontSize: 16,
@@ -126,54 +141,40 @@ const styles = StyleSheet.create({
     height: 20,
     flexDirection: 'row',
     gap: 4,
-    position: 'absolute',
-    top: 259,
-    left: 24,
   },
   changeButton: {
     width: 312,
     height: 40,
-    position: 'absolute',
-    top: 720,
-    left: 24,
     borderRadius: 6,
     backgroundColor: '#4D83F4',
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginLeft: 24,
+    marginBottom: 10,
   },
   buttonText: {
     fontSize: 16,
     fontWeight: '700',
     color: '#FFFFFF',
-    position: 'absolute',
-    top: 730,
-    left: 151,
-  },
-  timeRow: {
-    width: 312,
-    height: 78,
-    flexDirection: 'row',
-    gap: 6,
+    textAlign: 'center',
   },
   timeButton: {
     width: 100,
     height: 36,
-    padding: 8,
-    paddingHorizontal: 33,
     borderRadius: 6,
     borderWidth: 1,
     borderColor: '#C5C5C5',
-    opacity: 0,
+    opacity: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
   selectedTime: {
     width: 100,
     height: 36,
-    padding: 8,
-    paddingHorizontal: 33,
     borderRadius: 6,
     borderWidth: 1,
     borderColor: '#4D83F4',
-    opacity: 0,
+    opacity: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -188,5 +189,11 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: '#4D83F4',
     textAlign: 'center',
+  },
+  timeRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 4,
+    marginHorizontal: 25,
   },
 });
