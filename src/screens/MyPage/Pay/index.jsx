4b79275b85dfda42fbc8e20e4207508d4ed47a83
rudtj <kyoungseo03@naver.com>
2024-08-30 13:33:33 +0900

    feat: modal 추가

--- src/screens/MyPage/Pay/index.jsx
+++ src/screens/MyPage/Pay/index.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Modal,
+} from 'react-native';
 import Arrow from '@assets/images/register/arrowImg.png';
 import premium from '@assets/images/mypage/premium.png';
+import usual from '@assets/images/mypage/usual.png';
+import free from '@assets/images/mypage/free.png';
 
 export default function PayScreen({navigation}) {
-  //   const [nickname, setNickname] = useState('내가 둘째다');
+  const [showType, setShowType] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
-  //   const handleChange = async () => {
-  //     try {
-  //       await AsyncStorage.setItem('nickname', nickname);
-  //       navigation.goBack('MyPage');
-  //     } catch (error) {
-  //       console.error('닉네임 저장 실패:', error);
-  //     }
-  //   };
+  const handleChangeClick = () => {
+    setShowType(true);
+  };
+
+  const handlePopupCancelClick = () => {
+    setShowType(false);
+  };
+
+  const handlePress = imageType => {
+    setSelected(imageType);
+  };
+
+  const handleConfirm = () => {
+    setModalVisible(true);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
@@ -25,7 +43,7 @@ export default function PayScreen({navigation}) {
       </View>
       <View style={styles.typeContainer}>
         <Text style={styles.subtitle}>이용중인 타입</Text>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleChangeClick}>
           <Text style={styles.buttonText}>타입 변경하기</Text>
         </TouchableOpacity>
       </View>
@@ -54,6 +72,12 @@ export default function PayScreen({navigation}) {
         <Text style={styles.text}>※ 다음 예상 결제일은 8월 17일 입니다.</Text>
       </View>
 
+      <View style={styles.textContainer2}>
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
+          <Text style={styles.text2}>해지하기</Text>
+        </TouchableOpacity>
+      </View>
+
       <TouchableOpacity
         style={styles.changeButton}
         onPress={() => {
@@ -61,6 +85,65 @@ export default function PayScreen({navigation}) {
         }}>
         <Text style={styles.changeButtonText}>변경하기</Text>
       </TouchableOpacity>
+
+      {showType && (
+        <View style={styles.showTypeContainer}>
+          <TouchableOpacity
+            style={styles.clearButtonContainer}
+            onPress={handlePopupCancelClick}>
+            <Image
+              style={styles.clearButton}
+              source={require('../../../assets/images/button/clearbtn.png')}
+            />
+          </TouchableOpacity>
+          <View style={styles.imageContainer}>
+            <View
+              style={styles.freeContainer}
+              onPress={() => handlePress('free')}>
+              {selected === 'free' && <View style={styles.selectedBox} />}
+              <Image source={free} style={styles.free} />
+            </View>
+            <View
+              style={styles.usualContainer}
+              onPress={() => handlePress('usual')}>
+              {selected === 'usual' && <View style={styles.selectedBox} />}
+              <Image source={usual} style={styles.usual} />
+            </View>
+            <View style={styles.premiumContainer2}>
+              <Image source={premium} style={styles.premium2} />
+            </View>
+          </View>
+        </View>
+      )}
+
+      <Modal
+        transparent={true}
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalTitleContainer}>
+              <Text style={styles.modalTitle}>정말 해지하시겠습니까?</Text>
+            </View>
+            <Text style={styles.modalMessage}>
+              해지하게 되면{'\n'}가족 모두가 앱을 사용할 수 없습니다.
+            </Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={() => setModalVisible(false)}>
+                <Text style={styles.buttonText1}>취소</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.confirmButton}
+                onPress={handleConfirm}>
+                <Text style={styles.buttonText2}>확인</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -160,7 +243,6 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 6,
     backgroundColor: '#4D83F4',
-    marginTop: 270,
     marginLeft: 24,
     justifyContent: 'center',
     alignItems: 'center',
@@ -200,5 +282,149 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: '700',
     color: 'rgba(197, 197, 197, 0.8)',
+  },
+  textContainer2: {
+    marginTop: 130,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text2: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#C5C5C5',
+    textDecorationLine: 'underline',
+  },
+  showTypeContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 192,
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: -1,
+    },
+    shadowOpacity: 0.15,
+    shadowRadius: 8.4,
+    elevation: 5,
+  },
+  clearButtonContainer: {
+    marginTop: 8,
+    marginRight: 8,
+    alignSelf: 'flex-end',
+    height: 24,
+    width: 24,
+  },
+  clearButton: {
+    height: 24,
+    width: 24,
+  },
+  imageContainer: {
+    marginTop: 20,
+  },
+  freeContainer: {
+    marginLeft: 24,
+    marginBottom: 24,
+    position: 'relative',
+  },
+  free: {
+    width: 100,
+    height: 20,
+  },
+  usualContainer: {
+    marginLeft: 24,
+    marginBottom: 24,
+    position: 'relative',
+  },
+  usual: {
+    width: 50,
+    height: 20,
+  },
+  premiumContainer2: {
+    marginLeft: 24,
+    marginBottom: 24,
+  },
+  premium2: {
+    width: 78,
+    height: 20,
+  },
+  selectedBox: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#DBE6FD',
+    zIndex: -1,
+  },
+  modalBackground: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    width: 312,
+    height: 228,
+    borderRadius: 6,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalTitleContainer: {
+    marginBottom: 10,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#FF3434',
+    textAlign: 'center',
+  },
+  modalMessage: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#383838',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    gap: 10,
+  },
+  cancelButton: {
+    width: 136,
+    height: 40,
+    borderRadius: 38,
+    backgroundColor: '#EEEEEE',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText1: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#C5C5C5',
+    textAlign: 'center',
+  },
+  confirmButton: {
+    width: 136,
+    height: 40,
+    borderRadius: 38,
+    backgroundColor: '#FF3434',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText2: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#FFFFFF',
+    textAlign: 'center',
   },
 });
