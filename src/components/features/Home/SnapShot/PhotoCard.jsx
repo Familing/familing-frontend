52cc3c7d081430c1198bc48cc42d90ad43d34050
yunseong <333665@naver.com>
2024-08-31 14:44:14 +0900

    fix: 스냅샷 등록 로직 수정

--- src/components/features/Home/SnapShot/PhotoCard.jsx
+++ src/components/features/Home/SnapShot/PhotoCard.jsx
@@ -1,23 +1,29 @@
 import React, {useState} from 'react';
-import {View, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Image, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import plusbtn from '../../../../assets/images/photocard/plusbtn.png';
 import {CameraAlert} from '@/components/common/CameraAlert';
 import mom from '../../../../assets/images/photocard/photocard2.png';
 
-export const PhotoCard = ({profile, uploadImage}) => {
+export const PhotoCard = ({profile, uploadImage, isShowSnapshot}) => {
   const [alertVisible, setAlertVisible] = useState(false);
 
   return (
     <View>
-      {uploadImage === 'EMPTY' ? (
-        <TouchableOpacity
-          onPress={() => setAlertVisible(true)}
-          style={styles.card}>
-          <Image source={plusbtn} style={styles.addImage} />
-        </TouchableOpacity>
+      {isShowSnapshot ? (
+        uploadImage === 'EMPTY' ? (
+          <TouchableOpacity
+            onPress={() => setAlertVisible(true)}
+            style={styles.card}>
+            <Image source={plusbtn} style={styles.addImage} />
+          </TouchableOpacity>
+        ) : (
+          <View style={styles.card}>
+            <Image style={styles.cardImg} source={{uri: uploadImage}} />
+          </View>
+        )
       ) : (
-        <View style={styles.card}>
-          <Image style={styles.cardImg} source={{uri: uploadImage}} />
+        <View style={styles.content}>
+          <Text style={styles.description}>업로드 시간이 아니에요!</Text>
         </View>
       )}
       <CameraAlert
@@ -53,16 +59,17 @@ const styles = StyleSheet.create({
     borderRadius: 6,
   },
   content: {
-    width: '100%',
-    height: '100%',
+    width: 150,
+    height: 150,
     backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 6,
   },
   description: {
     fontSize: 12,
     fontWeight: '700',
-    color: '#CECECE',
+    color: '#C5C5C5',
   },
   addImage: {
     width: 21,
