00587a09abfa9bf96e76ce1b7edcc11b7a870e9b
yunseong <333665@naver.com>
2024-09-02 20:26:33 +0900

    feat: getLoveChat() 구현

---
+++ src/api/getLoveChat.jsx
@@ -0,0 +1,12 @@
+import {BASE_URL} from '@/util/base_url';
+import axios from 'axios';
+import React from 'react';
+
+export const getLoveChat = async originMsg => {
+  const response = await axios.post(`${BASE_URL}/api/v1/gpt/chat`, {
+    message: originMsg,
+  });
+  console.log('반환', response.data.result);
+  const loveMsg = response.data.result.replace(/^["']|["']$/g, '');
+  return loveMsg;
+};
