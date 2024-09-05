b3d0dc32c73d183ff23f95d7297b30edd2f84c17
yunseong0404 <333665@naver.com>
2024-09-05 21:30:17 +0900

    style: 불필요 주석제거

--- src/components/features/Home/SnapShot/FamilyPhotoCard.jsx
+++ src/components/features/Home/SnapShot/FamilyPhotoCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Image, Text, StyleSheet} from 'react-native';
-import mom from '../../../../assets/images/photocard/photocard2.png';
 
 export const FamilyPhotoCard = ({profile, snapshot}) => {
   return (
